3fa5b1f9227958dd1f640aaf6bb7fca0356a57d9
d-bizari <daniel.bizari@mercadolibre.com>
2019-10-31 12:20:04 -0300

    Primera version del test

---
+++ src/service/tweet_manager_test.go
@@ -0,0 +1,16 @@
+package service_test
+
+import (
+	"github.com/d-bizari/exampleGo/src/service"
+	"testing"
+)
+
+func TestPublishedTweetIsSaved(t *testing.T){
+	var tweet string = "This is my first tweet"
+
+	service.PublishTweet(tweet)
+
+	if service.Tweet != tweet {
+		t.Error("Expected is", tweet)
+	}
+}
